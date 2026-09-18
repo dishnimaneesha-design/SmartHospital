3bfd109b255f9218b7e3ea63bcb163805ad7e12e
dishnimaneesha-design <dishnimaneesha@gmail.com>
2026-09-18 21:38:49 +0530

    Initial project setup

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("========================================\n");
+    printf("       SMART HOSPITAL SYSTEM\n");
+    printf("========================================\n");
+
+    return 0;
+}
